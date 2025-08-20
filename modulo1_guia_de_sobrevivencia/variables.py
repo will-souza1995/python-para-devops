083e76d6225f06df7421db65b9ff2a4f49d2e676
Will Souza <williamsouza011995@gmail.com>
2025-08-20 09:13:12 -0300

    feat: Novas anotações

--- modulo1_guia_de_sobrevivencia/variables.py
+++ modulo1_guia_de_sobrevivencia/variables.py
@@ -1,1 +1,39 @@
-nome = "william"
+#Números Inteiro (int) - Usado para guardar valores inteiros (sem vírgula).
+idade = 29
+ano = 1995
+
+#Números decimais (float) - Usado para valores com vírgula (no Python se usa ponto .)
+altura = 1.75
+preco = 19.99
+
+#Texto (str) - Qualquer sequência de caracteres entre aspas simples ' ' ou aspas duplas " ".
+nome = "William"
+cidade = 'São Paulo'
+
+#Lógico / Booleano (bool) - Só pode ser True (verdadeiro) ou False (falso).
+ativo = True
+maior_de_idade = False
+
+#Listas (list) - Coleção de valores (podem ser de tipos diferentes).
+frutas = ["maçã", "banana", "uva"]
+numeros = [1, 2, 3, 4, 5]
+
+#Tuplas (tuple) - Parecidas com listas, mas não podem ser alteradas depois de criadas.
+coordenadas = (10, 20, 30)
+
+#Conjuntos (set) - Guarda valores sem repetição e sem ordem.
+cores = {"vermelho", "azul", "verde"}
+
+#Dicionários (dict) - Guarda pares chave: valor (como se fosse um “mapa”).
+pessoa = {
+    "nome": "William",
+    "idade": 25,
+    "cidade": "São Paulo"
+}
+
+
+#print("Meu nome é", nome, "moro na cidade de", cidade)
+#print(f"meu nome é {nome} eu moro na cidade de {cidade}")
+#print(f"O valor de {preco *2} é que você está devendo para o banco {nome}")
+
+
